e595ed51f1fe4a0904f998f962f4e202c04b9a5c
Tanya Yuferova <tanya_yu_95@mail.ru>
2017-11-15 23:20:07 -0500

    UI tests

---
+++ app/src/androidTest/java/com/example/tanyayuferova/bakingapp/MainActivityTest.java
@@ -0,0 +1,38 @@
+package com.example.tanyayuferova.bakingapp;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.tanyayuferova.bakingapp.ui.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by Tanya Yuferova on 11/15/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickRecipe_ShowsIngredientsAndSteps() {
+        // Click on first recipe
+        onView(withId(R.id.rv_recipes))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        // Ingredients and steps table should be displayed
+        onView(withId(R.id.rv_ingredients)).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_steps)).check(matches(isDisplayed()));
+    }
+}
---
+++ app/src/androidTest/java/com/example/tanyayuferova/bakingapp/RecipeActivityTest.java
@@ -0,0 +1,81 @@
+package com.example.tanyayuferova.bakingapp;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import com.example.tanyayuferova.bakingapp.ui.MainActivity;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+/**
+ * Created by Tanya Yuferova on 11/15/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void ingredientMeasureClick_ShowsToast() {
+        // Click on first recipe
+        onView(withId(R.id.rv_recipes))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        // Click on measure icon in first ingredient
+        onView(withId(R.id.rv_ingredients))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0,
+                        MyViewAction.clickChildViewWithId(R.id.iv_measure)));
+
+        // A toast message "Cup" should be displayed
+        onView(withText(R.string.cup_measure))
+                .inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+    }
+
+}
+
+/**
+ * Click on a child view with specified id
+ */
+class MyViewAction {
+
+    public static ViewAction clickChildViewWithId(final int id) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Click on a child view with specified id.";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                View v = view.findViewById(id);
+                v.performClick();
+            }
+        };
+    }
+
+}
